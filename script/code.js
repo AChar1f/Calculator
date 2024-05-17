2a1796254a6860e6b62478574d7874079c3614f9
AChar1f <acharif41@gmail.com>
2024-05-17 16:03:37 +0200

    ismaaeel

--- script/code.js
+++ script/code.js
@@ -1,5 +1,6 @@
 let dataEntry = document.querySelector('[data-entry]') 
 
 function display (value) {
-    dataEntry.textContent += value
+    dataEntry.value += value
 }
+//div has 2 types of content innerText and textContent to target the div we need to select the textContent. When using getById selector we need to make use of the innerText. (when using a div for display)
